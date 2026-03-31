98392e0598a2deecc69d63a6a8edcd1eec5672b3
mda-diaz <mdadiaz@gmail.com>
2026-03-31 13:24:04 +0200

    Update main.py

--- apps/api/main.py
+++ apps/api/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+from database import create_db_and_tables
 
 app = FastAPI()
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
 
 @app.get("/")
 def read_root():
